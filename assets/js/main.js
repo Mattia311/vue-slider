c67a0616044cee951a9a8ddfea1600084f8b33c5
Mattia311 <mattia.space98@gmail.com>
2021-10-29 12:41:23 +0200

    structure base

---
+++ assets/js/main.js
@@ -0,0 +1,35 @@
+const app = new Vue ({
+    el: '#app',
+    data: {
+        slides: [
+            {
+                items: 'img/01.jpg',
+                title: 'Svezia',
+                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+            },
+            {
+                items: 'img/02.jpg',
+                title: 'Svizzera',
+                text: 'Lorem ipsum',
+            },
+            {
+                items: 'img/03.jpg',
+                title: 'Gran Bretagna',
+                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+            },
+            {
+                items: 'img/04.jpg',
+                title: 'Germania',
+                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+            },
+            {
+                items: 'img/05.jpg',
+                title: 'Paradise',
+                text: 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+            },
+
+        ]
+    }
+})
+
+
